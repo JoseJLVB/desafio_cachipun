2d23c23a83a562771c7a561b423e39ef87ab8108
JOSE LUIS VALCARCEL BRAVO <josevalcarcel@entrearaucarias.com>
2021-11-28 18:19:27 -0300

    añadido el contador de partidas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,82 @@
-function cachipun(minimo, maximo)
-{
-    var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
-    return numero;
+var num1;
+var num2;
+var num3;
+var num4 = 0;
+
+num1 = prompt('Ingrese cantidad de partidas');
+
+while (true) {
+
+    if (num4 == num1) {
+        break;
+    }
+
+    function cachipun(minimo, maximo)
+    {
+        var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
+        return numero;
+    }
+    
+    var piedra = 0;
+    var papel = 1;
+    var tijera = 2;
+    
+    var opciones = ["Piedra", "Papel", "Tijera"];
+    
+    var num2;
+    var num3 = cachipun(0,2);
+    
+    num2 = prompt("Elige una de las siguientes opciones: \n -Piedra: 0\n -Papel: 1\n -Tijera: 2");
+    
+    alert("CPU eligió " + opciones[num3]);
+    
+    if(num2 == piedra)
+    {
+        if(num3 == piedra)
+        {
+            alert("Hay un EMPATE");
+        }
+        else if(num3 == papel)
+        {
+            alert("CPU ha GANADO esta partida");
+        }
+        else if(num3 == tijera)
+        {
+            alert("GANASTE esta partida");
+        }
+    }
+    else if(num2 == papel)
+    {
+        if(num3 == piedra)
+        {
+            alert("GANASTE esta partida");
+        }
+        else if(num3 == papel)
+        {
+            alert("Hay un EMPATE");
+        }
+        else if(num3 == tijera)
+        {
+            alert("CPU ha GANADO esta partida");
+        }
+    }
+    else if(num2 == tijera)
+    {
+        if(num3 == piedra)
+        {
+            alert("CPU ha GANADO esta partida");
+        }
+        else if(num3 == papel)
+        {
+            alert("GANASTE esta partida");
+        }
+        else if(num3 == tijera)
+        {
+            alert("Hay un EMPATE");
+        }
+    }  
+    
+    num4 +=1
 }
 
-var piedra = 0;
-var papel = 1;
-var tijera = 2;
-
-var opciones = ["Piedra", "Papel", "Tijera"];
-
-var opcionUsuario;
-var opcionMaquina = cachipun(0,2);
-
-opcionUsuario = prompt("Elige una de las siguientes opciones: \n -Piedra: 0\n -Papel: 1\n -Tijera: 2");
-
-alert("CPU eligió " + opciones[opcionMaquina]);
-
-if(opcionUsuario == piedra)
-{
-    if(opcionMaquina == piedra)
-    {
-        alert("Hay un EMPATE");
-    }
-    else if(opcionMaquina == papel)
-    {
-        alert("CPU ha GANADO esta partida");
-    }
-    else if(opcionMaquina == tijera)
-    {
-        alert("GANASTE esta partida");
-    }
-}
-else if(opcionUsuario == papel)
-{
-    if(opcionMaquina == piedra)
-    {
-        alert("GANASTE esta partida");
-    }
-    else if(opcionMaquina == papel)
-    {
-        alert("Hay un EMPATE");
-    }
-    else if(opcionMaquina == tijera)
-    {
-        alert("CPU ha GANADO esta partida");
-    }
-}
-else if(opcionUsuario == tijera)
-{
-    if(opcionMaquina == piedra)
-    {
-        alert("CPU ha GANADO esta partida");
-    }
-    else if(opcionMaquina == papel)
-    {
-        alert("GANASTE esta partida");
-    }
-    else if(opcionMaquina == tijera)
-    {
-        alert("Hay un EMPATE");
-    }
-}
+console.log ('FIN DEL JUEGO');
